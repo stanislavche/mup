02eaa8102919b7ecf060b01fd92f312d77bd0a62
stanislavche <stress_tn@mail.ru>
2021-02-08 15:52:35 +0200

    Updates

---
+++ static/js/main.1c8fe3d6.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpmup=this.webpackJsonpmup||[]).push([[0],{11:function(e,n,t){"use strict";t.r(n);var c=t(1),r=t.n(c),s=t(3),i=t.n(s),o=(t(8),t(9),t(0));var u=function(){return Object(o.jsx)("div",{className:"App",children:Object(o.jsx)("h1",{children:"\u041c\u0418\u0420"})})};i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(u,{})}),document.getElementById("root"))},8:function(e,n,t){},9:function(e,n,t){}},[[11,1,2]]]);
+//# sourceMappingURL=main.1c8fe3d6.chunk.js.map
