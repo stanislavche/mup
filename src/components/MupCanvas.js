633a34c132a30d03b5e08bd76722a89409338311
stanislavche <stress_tn@mail.ru>
2021-02-09 19:59:47 +0200

    add composition

--- src/components/MupCanvas.js
+++ src/components/MupCanvas.js
@@ -1,29 +1,38 @@
 import React, { useRef } from "react";
+import track1 from '../audio/promise.mp3';
 
 let frequencyArray = [];
 let analyser;
 let canvas;
 let ctx;
 let bars;
+const audio = new Audio();
 
 const MupCanvas = () => {
     const canvasRef = useRef(null);
     const requestRef = useRef(null);
 
     const handleInit = () => {
-        initAudio();
-        canvas = canvasRef.current;
-        ctx = canvas.getContext("2d");
-        ctx.lineWidth = 0.5;
-        ctx.lineCap = "round";
-        bars = Math.round(canvas.width);
-        requestRef.current = requestAnimationFrame(drawCanvas);
+        if (!audio.paused) {
+            audio.pause();
+        } else {
+            if (audio.src && audio.src.length) {
+                audio.play();
+            } else {
+                initAudio();
+                canvas = canvasRef.current;
+                ctx = canvas.getContext("2d");
+                ctx.lineWidth = 2;
+                ctx.lineCap = "round";
+                bars = Math.round(canvas.width);
+                requestRef.current = requestAnimationFrame(drawCanvas);
+            }
+        }
+
     };
 
     const initAudio = () => {
-        const audio = new Audio();
-        audio.src =
-            "https://s3.us-west-2.amazonaws.com/storycreator.uploads/ck9kpb5ss0xf90132mgf8z893?client_id=d8976b195733c213f3ead34a2d95d1c1";
+        audio.src = track1;
         audio.crossOrigin = "anonymous";
         audio.load();
 
@@ -57,17 +66,27 @@ const MupCanvas = () => {
     // dray lines around the circle
     const drawLine = (i, height, centerY, ctx) => {
         ctx.beginPath();
-        ctx.moveTo(i, centerY + height);
-        ctx.lineTo(i, centerY - height);
+        ctx.strokeStyle = '#dff8d0';
+        ctx.moveTo(i, centerY - height);
+        ctx.lineTo(i, centerY);
         ctx.stroke();
+
+        if (height < 200) {
+           drawStar(i, height, centerY, ctx);
+        }
     };
+
+    const drawStar = (i, height, centerY, ctx) => {
+        ctx.fillRect(i, height, 2, 2);
+        //console.log(i, height);
+    }
 
     return (
         <>
-            <button onClick={handleInit}>Start Visualizer</button>
+            <button onClick={handleInit}>Start</button>
             <canvas
                 ref={canvasRef}
-                style={{ background: "#f5f5f5" }}
+                style={{ background: "#071820" }}
                 width={window.innerWidth}
                 height={window.innerHeight}
             />
