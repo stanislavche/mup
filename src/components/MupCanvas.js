f60e50098f455efd16675537fc825bc8a5fa2f8c
stanislavche <stress_tn@mail.ru>
2021-03-23 22:40:42 +0200

    bind some events

--- src/components/MupCanvas.js
+++ src/components/MupCanvas.js
@@ -1,11 +1,17 @@
 import React, { useRef } from "react";
 import track1 from '../audio/promise.mp3';
+import boy from '../image/boy.svg';
 
-let frequencyArray = [];
-let analyser;
-let canvas;
-let ctx;
+let frequencyArray = [],
+    analyser,
+    canvas,
+    ctx,
+    isPlaying,
+    timeout;
 const audio = new Audio();
+const image = new Image();
+
+
 
 const MupCanvas = () => {
     const canvasRef = useRef(null);
@@ -18,57 +24,69 @@ const MupCanvas = () => {
             if (audio.src && audio.src.length) {
                 audio.play();
             } else {
+                initImage();
                 initAudio();
-                canvas = canvasRef.current;
-                ctx = canvas.getContext("2d");
-                requestRef.current = requestAnimationFrame(updateVisualization);
             }
         }
 
     };
 
+    const initImage = () => {
+        image.src = boy;
+    }
+
+    const initCanvas = () => {
+        canvas = canvasRef.current;
+        ctx = canvas.getContext("2d");
+        requestRef.current = requestAnimationFrame(updateVisualization);
+    };
+
     const initAudio = () => {
+        audio.addEventListener("pause", pauseEvent);
+        audio.addEventListener("play", playEvent);
         audio.src = track1;
         audio.crossOrigin = "anonymous";
         audio.load();
-
-        const context = new (window.AudioContext || window.webkitAudioContext)();
-        analyser = context.createAnalyser();
+        const audioContext = new (window.AudioContext || window.webkitAudioContext)();
+        analyser = audioContext.createAnalyser();
         analyser.fftSize = 512;
-        const source = context.createMediaElementSource(audio);
-
+        const source = audioContext.createMediaElementSource(audio);
         source.connect(analyser);
-        analyser.connect(context.destination);
-
+        analyser.connect(audioContext.destination);
         frequencyArray = new Uint8Array(analyser.frequencyBinCount);
         audio.play();
     };
 
+    const pauseEvent = () => {
+        timeout = setTimeout(()=>{
+            isPlaying = false;
+        },2000);
+    }
+    const playEvent = () => {
+        clearTimeout(timeout);
+        isPlaying = true;
+        initCanvas();
+    }
+
     // draw the whole thing
-    const drawCanvas = (frequencyArray) => {
+    const drawCanvas = () => {
         if (canvasRef.current) {
-            ctx.clearRect(0, 0, canvas.width, canvas.height);
-            ctx.save();
-            //ctx.globalCompositeOperation='source-over';
-            ctx.scale(0.5, 0.5);
-            ctx.translate(window.innerWidth, window.innerHeight);
-
-
-            var bass = Math.floor(frequencyArray[1]); //1Hz Frequenz
-            var radius = 0.45 * canvas.width <= 450 ? -(bass * 0.25 + 0.45 * canvas.width) : -(bass * 0.25 + 300);
-            for (var i = 0; i < frequencyArray.length; i++) {
+            analyser.getByteFrequencyData(frequencyArray);
+            let bass = Math.floor(frequencyArray[1]); //1Hz Frequenz
+            let radius = 0.45 * canvas.width <= 450 ? -(bass * 0.25 + 0.45 * canvas.width) : -(bass * 0.25 + 100);
+            for (let i = 0; i < frequencyArray.length; i++) {
                 const position = frequencyArray[i];
                 if (i > 0) {
                     ctx.fillStyle = '#86c06c';
-                    ctx.fillRect(0, radius, 4, -position);
+                    ctx.fillRect(0, radius, 3, -position/2);
                     ctx.rotate((180 / 128) * Math.PI / 180);
                 }
             }
-            for (var i = 0; i < frequencyArray.length; i++) {
+            for (let i = 0; i < frequencyArray.length; i++) {
                 const position = frequencyArray[i];
                 if (i > 0) {
                     ctx.fillStyle = '#316851';
-                    ctx.fillRect(0, radius, 3, -position);
+                    ctx.fillRect(0, radius, 3, -position/2);
                     ctx.rotate(-(180 / 128) * Math.PI / 180);
                 }
             }
@@ -77,9 +95,21 @@ const MupCanvas = () => {
     };
 
     function updateVisualization () {
-        analyser.getByteFrequencyData(frequencyArray);
-        drawCanvas(frequencyArray);
-        requestRef.current = requestAnimationFrame(updateVisualization);
+        if (isPlaying) {
+            if (canvasRef.current) {
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                ctx.save();
+                ctx.globalCompositeOperation = 'color-dodge';
+                ctx.translate(window.innerWidth / 2, window.innerHeight / 2);
+                drawImage();
+                drawCanvas();
+                requestRef.current = requestAnimationFrame(updateVisualization);
+            }
+        }
+    };
+
+    const drawImage = () => {
+        ctx.drawImage(image, canvas.width/2 - 150, canvas.height/2 - 150, 300, 300);
     };
 
     return (
